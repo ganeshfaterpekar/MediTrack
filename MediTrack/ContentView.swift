7295100c2755bfdc051892abf3e227f1475de609
Ganesh Faterpekar <ganeshfaterpekar@gmail.com>
2026-01-12 10:22:07 +1100

    Initial Commit

---
+++ MediTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediTrack
+//
+//  Created by Ganesh Faterpekar on 12/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediTrack/MediTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediTrackApp.swift
+//  MediTrack
+//
+//  Created by Ganesh Faterpekar on 12/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MediTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
