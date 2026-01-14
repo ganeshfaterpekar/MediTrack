9e1f08ea711cc40a5b2edd51341ae2bbfdcbd22c
Ganesh Faterpekar <ganeshfaterpekar@gmail.com>
2026-01-15 10:50:50 +1100

    Presentation - ROW UI

---
+++ MediTrack/Presentation/UILibray/MedicationUI.swift
@@ -0,0 +1,15 @@
+//
+//  MedicationUI.swift
+//  MediTrack
+//
+//  Created by Ganesh Faterpekar on 13/1/2026.
+//
+
+import HEUILibrary
+
+enum MedicationUI {
+  
+   static func makeMedicationListRowItem(medication: Medication) -> ThumbnailDescriptionView {
+        return ThumbnailDescriptionView(name: medication.name, dosage: medication.dosage, frequency: medication.frequency.displayTitle, image: "pills")
+    }
+}
